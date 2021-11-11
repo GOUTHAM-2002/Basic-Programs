4b16a263842a108b6eb44a8aac9d56def7ead8ca
GOUTHAM-2002 <goutham3336@gmail.com>
2021-11-11 23:27:23 +0530

    init

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+import json
+
+data = json.load(open('data.json'))
+
+
+def dict(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist"
+
+
+word = input("Enter words: ")
+
+print(dict(word))
